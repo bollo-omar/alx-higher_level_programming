9987e6dee05e2e8e65ac5ebe735b9b8b98f61802
bollo-omar <bolloaggrey@gmail.com>
2023-02-14 13:07:26 -0800

    c4eate table and add 4 records

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,6 @@
 -- Creates and fills a table second_table with attributes id, name and score.
-CREATE TABLE IF NOT EXISTS second_table(id INT,name VARCHAR(256),score INT);
-INSERT INTO second_table(id,name, score) VALUES
-(id = 1, name = "John", score = 10),
-(id = 2, name = "Alex", score = 3),
-(id = 3, name = "Bob", score = 14),
-(id = 4, name = "George", score = 8);
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
