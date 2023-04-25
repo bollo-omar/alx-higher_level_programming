e459e5cccebd894f2bb4efa65ee4d6bf2eacb9f8
bollo-omar <bolloaggrey@gmail.com>
2023-04-25 20:09:59 +0000

    task 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,14 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
-  }
-});
 
-function printCharacters (characters, index) {
+function printCharacters(characters, index) {
   request(characters[index], function (error, response, body) {
     if (!error) {
       console.log(JSON.parse(body).name);
@@ -17,3 +11,12 @@ function printCharacters (characters, index) {
       }
     }
   });
+}
+
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  }
+});
+
